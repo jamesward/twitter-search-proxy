1fcf96de91cf202d87ae82f7b1b4a22dfaea2bf6
Stefano S <kindoblue@gmail.com>
2015-06-21 18:56:46 +0200

    project updated to play 2.4.0

--- app/controllers/RandomFailures.scala
+++ app/controllers/RandomFailures.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api.mvc._
 import play.api.libs.concurrent.Promise
+import scala.concurrent.Future
 import scala.util.Random
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -9,11 +10,11 @@ import play.api.Logger
 
 case class FailFast[A](action: Action[A]) extends Action[A] with Controller {
 
-  def apply(request: Request[A]): Result = {
+  def apply(request: Request[A]): Future[Result] = {
     // fail about once in every 10 times
     if (Random.nextInt(10) == 0) {
       Logger.info("FailFast")
-      InternalServerError
+      Future(InternalServerError)
     } else {
       action(request)
     }
@@ -24,15 +25,12 @@ case class FailFast[A](action: Action[A]) extends Action[A] with Controller {
 
 case class WaitOneMinute[A](action: Action[A]) extends Action[A] with Controller {
 
-  def apply(request: Request[A]): Result = {
+  def apply(request: Request[A]): Future[Result] = {
     // wait one minute about once in every 10 times
     if (Random.nextInt(10) == 0) {
       Logger.info("WaitOneMinute")
-      Async {
-        Promise.timeout(RequestTimeout, 1 minute)
-      }
-    }
-    else {
+      Promise.timeout(RequestTimeout, 1 minute)
+    } else {
       action(request)
     }
   }
--- app/utils/Twitter.scala
+++ app/utils/Twitter.scala
@@ -1,9 +1,9 @@
 package utils
 
 import play.api.{Logger, Play}
-import play.api.libs.ws.{Response, WS}
-import com.ning.http.client.Realm.AuthScheme
+import play.api.libs.ws.{WSAuthScheme, WSResponse, WS}
 import scala.concurrent.{Promise, Future}
+import play.api.Play.current
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Twitter {
@@ -38,13 +38,13 @@ object Twitter {
     require(Play.current.configuration.getString("twitter.consumer.secret").isDefined)
     
     WS.url("https://api.twitter.com/oauth2/token")
-      .withAuth(Play.current.configuration.getString("twitter.consumer.key").get, Play.current.configuration.getString("twitter.consumer.secret").get, AuthScheme.BASIC)
+      .withAuth(Play.current.configuration.getString("twitter.consumer.key").get, Play.current.configuration.getString("twitter.consumer.secret").get, WSAuthScheme.BASIC)
       .post(Map("grant_type" ->  Seq("client_credentials")))
       .withFilter(response => (response.json \ "token_type").asOpt[String] == Some("bearer"))
       .map(response => (response.json \ "access_token").as[String])
   }
 
-  def fetchTweets(bearerToken: String, query: String): Future[Response] = {
+  def fetchTweets(bearerToken: String, query: String): Future[WSResponse] = {
     WS.url("https://api.twitter.com/1.1/search/tweets.json")
       .withQueryString("q" -> query)
       .withHeaders("Authorization" -> s"Bearer $bearerToken")
